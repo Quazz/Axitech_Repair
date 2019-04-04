ef2aa3498f7e896b6ffd55d67783d37812566e1d
Quazz <nickverschragen@gmail.com>
2019-04-04 16:03:14 +0200

    Update Ticket.php

--- Model/ResourceModel/Ticket.php
+++ Model/ResourceModel/Ticket.php
@@ -1,10 +1,10 @@
 <?php
-namespace Jeff\Helpdesk\Model\ResourceModel;
+namespace Axitech\Repair\Model\ResourceModel;
 
 use Magento\Framework\Model\ResourceModel\Db\AbstractDb; 
 
 class Ticket extends AbstractDb {
     protected function _construct() {
-        $this->_init('jeff_helpdesk_ticket', 'ticket_id');
+        $this->_init('axitech_repair_ticket', 'ticket_id');
     }
 }
