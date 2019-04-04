d2a2ef4093aca8718c249a2c5229e9110e97d0a5
Quazz <nickverschragen@gmail.com>
2019-04-04 15:49:38 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
     \Magento\Framework\Component\ComponentRegistrar::register(
         \Magento\Framework\Component\ComponentRegistrar::MODULE,
-        'Jeff_Helpdesk',
+        'Axitech_Repair',
         __DIR__
     );
