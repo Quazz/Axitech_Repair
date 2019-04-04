b30d88fee077ba8cbcbd45d4c29dee9bf07c41b1
Quazz <nickverschragen@gmail.com>
2019-04-04 15:02:53 +0200

    Update Ticket.php

--- Model/Ticket.php
+++ Model/Ticket.php
@@ -4,10 +4,10 @@ namespace Jeff\Helpdesk\Model;
 class Ticket extends \Magento\Framework\Model\AbstractModel {
     const STATUS_OPENED = 1;
     const STATUS_CLOSED = 2;
-	const STATUS_DIAGNOSE = 3;
-	const STATUS_PART = 4;
-	const STATUS_PICKUP = 5;
-	const STATUS_INPROGRESS = 6;
+    const STATUS_DIAGNOSE = 3;
+    const STATUS_PART = 4;
+    const STATUS_PICKUP = 5;
+    const STATUS_INPROGRESS = 6;
 
     protected static $statusesOptions = [self::STATUS_OPENED=>'Opened', self::STATUS_CLOSED=>'Closed', self::STATUS_DIAGNOSE=>'Waiting for Diagnosis', self::STATUS_PART=>'Waiting for Parts', self::STATUS_PICKUP=>'Ready for Pickup', self::STATUS_INPROGRESS=>'In progress'];
 
@@ -19,8 +19,6 @@ class Ticket extends \Magento\Framework\Model\AbstractModel {
     {
         $this->_init('Jeff\Helpdesk\Model\ResourceModel\Ticket');
     }
-
-
 
     public static function getStatusesOptionArray() {
         return self::$statusesOptions;
