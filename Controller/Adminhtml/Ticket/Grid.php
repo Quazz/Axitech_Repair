84f77123617c7acec19981bc6a820631ffd6f805
Quazz <nickverschragen@gmail.com>
2019-04-04 16:20:46 +0200

    Update Grid.php

--- Controller/Adminhtml/Ticket/Grid.php
+++ Controller/Adminhtml/Ticket/Grid.php
@@ -9,9 +9,9 @@ class Grid extends \Axitech\Repair\Controller\Adminhtml\Ticket {
 }
 /*
 However, if we now try to use sorting or filtering, we would get a broken layout.
-This is because based on arguments defined under the jeff_helpdesk_ticket_grid_block.xml file,
+This is because based on arguments defined under the axitech_repair_ticket_grid_block.xml file,
 we are missing the controller Grid action. 
 When we use sorting or filtering, the AJAX request hits the Index controller Grid Action.
 The code within thex execute method simplye calls the loadLayout(false) method to prevent
-the entire layout loading, making it load only the bits defined under the jeff_helpdesk_ticket_grid.xml fiel.
+the entire layout loading, making it load only the bits defined under the axitech_repair_ticket_grid.xml fiel.
 */
