e655059f16edfe1dcd44cafa05a364738f7f0229
Quazz <nickverschragen@gmail.com>
2019-04-04 16:07:21 +0200

    Update Index.php

--- Controller/Ticket/Index.php
+++ Controller/Ticket/Index.php
@@ -1,14 +1,14 @@
 <?php
-namespace Jeff\Helpdesk\Controller\Ticket;
+namespace Axitech\Repair\Controller\Ticket;
 
-class Index extends \Jeff\Helpdesk\Controller\Ticket
+class Index extends \Axitech\Repair\Controller\Ticket
 {
 
     public function execute()
     {
         $resultPage = $this->resultFactory->create(\Magento\Framework\Controller\ResultFactory::TYPE_PAGE);
         
-        $resultPage->getConfig()->getTitle()->set(__('Help Tickets'));
+        $resultPage->getConfig()->getTitle()->set(__('Repair Tickets'));
         return $resultPage;
     }
 }
